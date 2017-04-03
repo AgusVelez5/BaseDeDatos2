52a06dad067d1bb85550a005e0f4dc16116b7a4d
AgusVelez5 <agustinmaurasvelez@gmail.com>
2017-04-03 13:35:57 -0300

    Clase 6

--- Clase6.sql
+++ Clase6.sql
@@ -33,29 +33,46 @@ WHERE EXISTS(SELECT *
 			AND f1.customer_id <> f2.customer_id)
 			
 -- Ejercicio 5
-			
-SELECT first_name, last_name, title
-FROM actor, film 
-WHERE title LIKE '%BETRAYED REAR%' OR title LIKE '%CATCH AMISTAD%'
+
+SELECT DISTINCT actor_id
+FROM actor f1
+WHERE actor_id IN (SELECT f3.actor_id
+					FROM film f2, film_actor f3
+					WHERE f3.film_id = f2.film_id AND title LIKE '%BETRAYED REAR%' AND f1.actor_id = f3.actor_id)
+    OR actor_id IN (SELECT f3.actor_id
+	    				  FROM film f2, film_actor f3
+		    			  WHERE f3.film_id = f2.film_id AND title LIKE '%CATCH AMISTAD%' AND f1.actor_id = f3.actor_id)
 
 -- Ejercicio 6
 
-SELECT first_name, last_name, title
-FROM actor 
-JOIN film
-ON title LIKE '%BETRAYED REAR%' AND NOT title LIKE '%CATCH AMISTAD%'
+SELECT DISTINCT actor_id
+FROM actor f1
+WHERE actor_id IN (SELECT f3.actor_id
+					FROM film f2, film_actor f3
+					WHERE f3.film_id = f2.film_id AND title LIKE '%BETRAYED REAR%' AND f1.actor_id = f3.actor_id)
+    AND actor_id NOT IN (SELECT f3.actor_id
+	    				  FROM film f2, film_actor f3
+		    			  WHERE f3.film_id = f2.film_id AND title LIKE '%CATCH AMISTAD%' AND f1.actor_id = f3.actor_id)
 
--- Ejercio 7
+-- Ejercicio 7
 
-SELECT first_name, last_name, title
-FROM actor
-JOIN film
-ON title LIKE '%BETRAYED REAR%' AND title LIKE '%CATCH AMISTAD%'
+SELECT DISTINCT actor_id
+FROM actor f1
+WHERE actor_id IN (SELECT f3.actor_id
+					FROM film f2, film_actor f3
+					WHERE f3.film_id = f2.film_id AND title LIKE '%BETRAYED REAR%' AND f1.actor_id = f3.actor_id)
+    AND actor_id IN (SELECT f3.actor_id
+	    				  FROM film f2, film_actor f3
+		    			  WHERE f3.film_id = f2.film_id AND title LIKE '%CATCH AMISTAD%' AND f1.actor_id = f3.actor_id)
 
 -- Ejercicio 8
-
-SELECT DISTINCT first_name, last_name
-FROM actor
-JOIN film
-ON NOT title LIKE '%BETRAYED REAR%' AND NOT title LIKE '%CATCH AMISTAD%'
-
+					
+SELECT DISTINCT actor_id
+FROM actor f1
+WHERE actor_id NOT IN (SELECT f3.actor_id
+					FROM film f2, film_actor f3
+					WHERE f3.film_id = f2.film_id AND title LIKE '%BETRAYED REAR%' AND f1.actor_id = f3.actor_id)
+    AND actor_id NOT IN (SELECT f3.actor_id
+	    				  FROM film f2, film_actor f3
+		    			  WHERE f3.film_id = f2.film_id AND title LIKE '%CATCH AMISTAD%' AND f1.actor_id = f3.actor_id)
+		    			  ORDER BY 1
