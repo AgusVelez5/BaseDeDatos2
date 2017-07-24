f032e90966fd0046d29fa1c710b8f31a369cc9bc
AgusVelez5 <agustinmaurasvelez@gmail.com>
2017-07-24 20:48:15 -0300

    Clase 14

---
+++ Clase11.sql
@@ -0,0 +1,38 @@
+
+#Ejercicio 1
+SELECT DISTINCT country, (SELECT COUNT(city)
+						  FROM city 
+						  WHERE country.country_id = city.country_id) AS total
+FROM country 
+ORDER BY country.country_id 
+
+SELECT country, COUNT(*) AS total
+FROM country
+INNER JOIN city USING(country_id)
+GROUP BY country
+ORDER BY country.country_id
+
+#Ejercicio 2
+SELECT country, COUNT(*)
+FROM country
+INNER JOIN city USING(country_id)
+GROUP BY country
+HAVING COUNT(*) > 10
+ORDER BY country.country_id
+
+#Ejercicio3
+SELECT first_name, last_name, address, COUNT(rental.rental_id) AS Total_Alquiladas, SUM(amount) AS Saldo_Gastado
+FROM customer
+JOIN address USING (address_id)
+JOIN rental USING (customer_id)
+JOIN payment USING (rental_id)
+GROUP BY 1, 2, 3
+
+#Ejercicio 4
+
+
+
+SELECT *
+  FROM film AS f
+      INNER JOIN `language` AS l
+              ON f.language_id = l.language_id
