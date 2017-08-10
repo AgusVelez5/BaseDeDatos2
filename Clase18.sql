d5e0a593afc3b3a9043e4ff47d5f624c43f01017
AgusVelez5 <agustinmaurasvelez@gmail.com>
2017-08-10 18:03:19 -0300

    Clase 18

--- Clase18.sql
+++ Clase18.sql
@@ -2,3 +2,19 @@ CALL GetOfficeByCountry('USA');
 
 CALL CountOrderByStatus('Shipped',@total);
 SELECT @total;
+
+CALL get_order_by_cust(141,@shipped,@canceled,@resolved,@disputed);
+SELECT @shipped,@canceled,@resolved,@disputed;
+
+
+SET @customerNo = 112;
+
+SELECT country into @country
+FROM customers
+WHERE customernumber = @customerNo;
+
+CALL GetCustomerShipping(@customerNo,@shipping);
+
+SELECT @customerNo AS Customer,
+       @country    AS Country,
+       @shipping   AS Shipping;
